0b861e670b52e32cf4fff621ea3aee94291fc2a0
sspano <sspano@colgate.edu>
2015-09-20 16:55:22 -0400

    Updated game components

--- test.c
+++ test.c
@@ -28,50 +28,71 @@ wordnode *list_insert(char *word, wordnode *head){
 
 
 
-int check_guess(bool* guess_list, char guess) {
+int check_guess(bool* guess_list, char* current_guess) {
   /*
   Given a list of guessed letters (organized by ASCII value to index) this function sees if the guess has already been guessed. If it has, it returns a 0, if it is a new guess, this function returns a 1.
   */
-  // convert char to ascii value
-  // checks that the character is within the ranges for lower or uppercase letters (if not returns 0)
-  // converts to uppercase (adds some int to int value)
-  // subtracts value of 'A' to get index value
-  // sees if that guess_list[index] == 0 if so valid guess, change value to 0
-  // otherwise invalid guess, return 0
+  
   // later on we can add complexity where we return different numbers to indicate different reasons for invalidity (eg. 1 means already guessed 2 means non-alpha char)
-  return 0;
+    
+  printf("Checking guess...\n");
+  if (strlen(current_guess) !=1) {
+    return 0;
+  }
+  
+  *current_guess = toupper(*current_guess);
+  if (*current_guess <= 'Z' && *current_guess >= 'A') {
+    printf("Guess is a character.\n");
+    int index = *current_guess - 'A';
+    if (guess_list[index]) {
+      return 0;
+    }
+    else {
+      printf("Valid guess\n");
+      guess_list[index] = true;
+      return 1;
+    }
+  }
+  else {
+    return 0;
+  }
 }
 
 char prompt_guess(bool* guess_list){
   /*
   Prompts user for a guess and returns a valid guess. If invalid (checks validity by calling check_guess), prompts user continuously for a valid guess.
   */
+  int buffer = 256;
   char current_guess = 'A';
   printf ("Enter a character: ");
-  scanf ("%c", &current_guess); // need to change the character to upper case
+  fgets(&current_guess, buffer*sizeof(char), stdin);
   printf ("You entered: %c\n", current_guess);
-  int valid_guess = 1;
-//  int valid_guess = check_guess(guess_list, current_guess);
-  while (!valid_guess){
-    //prompt
-    printf("Invalid Guess. You have either guessed this letter already or entered a non-alphabetic single character. Please enter a new character.");
-    scanf ("%c", &current_guess);
+  int valid_guess = check_guess(guess_list, &current_guess);
+  while (valid_guess != 1){
+    current_guess = 'A';
+    printf("Invalid Guess. You have either guessed this letter already or entered a non-alphabetic character. \nPlease enter a new character :\t");
+    fgets(&current_guess, buffer*sizeof(char), stdin);
+    current_guess = toupper(current_guess);
     printf ("You entered: %c\n", current_guess);
+    valid_guess = check_guess(guess_list, &current_guess);
   }
-  return 0;
+  return current_guess;
 }
 
 
 
 
 int main() {
- 
   char guess;
-  bool guess_list[126] = {0};
-  guess = prompt_guess(guess_list);
-  
-  return 0;
-  
+  int guess_count = 0;
+  int max_guesses = 7;
+  bool guess_list[26] = {false};
+  if (guess_count < max_guesses) {
+    guess = prompt_guess(guess_list);
+    printf("Return to main guess value : %c\n", guess);
+    printf("Value of %c in guess list (should be true) : %d\n", guess, guess_list[guess - 'A']);
+  }
+  return 0; 
 }
 
 
